ffde251ec708b182e170e5ba5815411b8a7f111e
Tauqeer Ahmed <freelancingtwin@gmail.com>
2022-11-09 14:34:36 +0530

    use match.regex inside sort to order words that nest numbers

---
+++ 9nov22SortHard.js
@@ -0,0 +1,24 @@
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+Examples
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+
+function order(words){
+    //words is a string with words containing integers
+    //sort those words according to their nested integers
+    //"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+//use regex to find the numbers, then sort. 
+//correction! use regex inside sort
+//use match with regex
+  return words.split(' ').sort((a,b)=> {
+     return a.match(/\d/) - b.match(/\d/)
+  }).join(' ')
+}
