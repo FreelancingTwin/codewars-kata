c7ed245bfa59858e23a182ddf62954e2b8a6971b
Tauqeer Ahmed <freelancingtwin@gmail.com>
2022-12-12 15:44:36 +0530

    maps and reduce, more practice

---
+++ 12dec22map.js
@@ -0,0 +1,41 @@
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 1000
+    -10^6 <= nums[i] <= 10^6
+
+
+nums.reduce((a, b, idx, arr)=> arr[idx]+= a)
+   return nums
+
+   for(let el = 1; el < nums.length; el++){
+       nums[el]+= nums[el - 1]
+   } return nums
+
+   //maps method
+let counter = 0
+   return nums.map(el=> counter+= el)
