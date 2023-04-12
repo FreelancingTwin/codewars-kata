861e5c4e70ebc04d6bd35e3a311db60ca49e93b7
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-04-12 14:21:49 +0530

    more python basics

--- 13apr23pythonDefs.py
+++ 13apr23pythonDefs.py
@@ -1,20 +1,20 @@
-Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+# Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
-Please use the following function names:
+# Please use the following function names:
 
-addition = add
+# addition = add
 
-multiply = multiply
+# multiply = multiply
 
-division = divide (both integer and float divisions are accepted)
+# division = divide (both integer and float divisions are accepted)
 
-modulus = mod
+# modulus = mod
 
-exponential = exponent
+# exponential = exponent
 
-subtraction = subt
+# subtraction = subt
 
-Note: All math operations will be: a (operation) b
+# Note: All math operations will be: a (operation) b
 
 def add(a,b): return a+b
 def multiply(a,b): return a*b
@@ -23,6 +23,12 @@ def mod(a,b): return a%b
 def exponent(a,b): return a^b
 def subt(a,b): return a-b
     
+add(2,5)
+multiply(6,4)
+divide(3,6)
+mod(6,3)
+exponent(6,7)
+subt(5,5)
 # Make more functions. Refer to the description for function names.
 # The code will NOT WORK IF YOU USE names other than the ones
 # from the description
