8e77d9ab3bf1a6274048d37b35d17d03638d953b
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-04-24 14:20:35 +0530

    some python operators debugging

---
+++ 24apr23comparision.py
@@ -0,0 +1,14 @@
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+# a == "dev"
+# b == "Lab"
+
+# name == a + b
+
+# here, its using the comparison operator == instead of the assignment operator =, change that
+
+a = "dev"
+b = "Lab"
+
+name = a + b
