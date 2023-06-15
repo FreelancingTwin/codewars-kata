90cf2105a1e24e9c115a80de4c3ad8c1e60f0873
Tauqeer Ahmed <freelancingtwin@gmail.com>
2023-06-15 23:04:32 +0530

    simple variation of fizzbuzz

---
+++ karthikinterview2.js
@@ -0,0 +1,17 @@
+// Write a program that prints the numbers from 1 to 100. But for multiples of three, print "Muhammed" instead of the number, and for multiples of five, print "Tauqeer". For numbers that are multiples of both three and five, print "Muhammed  Tauqeer".
+
+// const isName = () => {
+//     for(let i = 1; i <= 100; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log("Muhammed Tauqeer")
+//         } else if(i % 3 === 0){
+//             console.log('Muhammed')
+//         } else if( i % 5 === 0){
+//             console.log('Tauqeer')
+//         }  else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// isName()
